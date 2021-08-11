9390165bb388e17ecf5e7e7649077c5470ccea29
stepanbacho <bachostepan@gmail.com>
2021-08-11 20:56:27 +0300

    added styles and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { BasketComponent } from './pages/basket/basket.component';
+import { PaymentComponent } from './pages/payment/payment.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {path: 'products', component: ProductsComponent},
+  {path: 'products/:name', component: ProductDetailsComponent },
+  { path: 'basket', component: BasketComponent },
+  { path: 'payment', component: PaymentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { BasketComponent } from './pages/basket/basket.component';
+import { PaymentComponent } from './pages/payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductDetailsComponent,
+    BasketComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
